873c24dd79700e16167eb4833333531e1e827576
Aditya Doke <aditya.doke@gmail.com>
2016-02-10 14:55:50 +0530

    meta change

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name 'wrapper-nginx'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+name 'tal-nginx'
+maintainer 'Aditya Doke'
+maintainer_email 'adityadoke@talentica.com'
 license 'all_rights'
-description 'Installs/Configures wrapper-nginx'
-long_description 'Installs/Configures wrapper-nginx'
+description 'Installs/Configures nginx'
+long_description 'Installs/Configures nginx'
 version '0.1.0'
 
 
